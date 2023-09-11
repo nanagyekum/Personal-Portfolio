29a34cb73b4fcb7f7a28cd88f0e1b36f5c58accd
Nana Agyekum <140916581+nanagyekum@users.noreply.github.com>
2023-09-10 21:45:57 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var typed = new Typed(".alotText", {
-  strings: ["Frontend Developer", "Actor", "Public Speaker", "TV Personality"],
+  strings: [
+    "Frontend Developer",
+    "Actress",
+    "Public Speaker",
+    "TV Personality",
+  ],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
